77f5001b0be59dc79a6608bfeb3aa1f29d6c2aed
Nicole Osborn <nicole.y.osborn@gmail.com>
2020-07-19 20:39:35 -0700

    updated dotenv to deploy to heroku

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const server = require('./server.js');
 
-const PORT = 8000;
-server.listen(PORT, () => {
-    console.log(`\n*** Server Running on http://localhost:${PORT} ***\n`);
+const port = process.env.PORT || 8000;
+server.listen(port, () => {
+    console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
 
 /*
